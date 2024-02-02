cb0bd7dfc5ac80bee89ec9faa7adc0e1077e9a97
dealpax <info@todaymind.com>
2024-02-02 15:44:55 +0530

    sideNavbar or searchBar done

---
+++ assets/js/custom.js
@@ -0,0 +1,13 @@
+// canvas menu
+function canvasBar() {
+    var sideBar = document.getElementById("sideBar");
+    sideBar.classList.toggle("max-w-[300px]");
+} 
+
+// Search Bar
+function searchBar() {
+    var sideBar = document.getElementById("searchBar"); 
+    sideBar.classList.toggle("hidden");
+} 
+
+ 
