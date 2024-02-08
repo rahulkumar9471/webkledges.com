cf700e947d57632c350bc018847f90a878b136ed
dealpax <info@todaymind.com>
2024-02-08 17:53:25 +0530

    carousel

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,4 +10,28 @@ function searchBar() {
     sideBar.classList.toggle("hidden");
 } 
 
- 
+//  Ccarousel
+
+let slideIndex = 0;
+
+function showSlide(n) {
+    const slides = document.querySelectorAll('.carousel-item');
+    if (n >= slides.length - 1) {
+        slideIndex = 0;
+    } else if (n < 0) {
+        slideIndex = slides.length - 2;
+    }
+    slides.forEach((slide, index) => {
+        slide.style.transform = `translateX(-${slideIndex * 100}%)`;
+    });
+}
+
+function nextSlide() {
+    showSlide(++slideIndex);
+}
+
+function prevSlide() {
+    showSlide(--slideIndex);
+}
+
+showSlide(slideIndex);
